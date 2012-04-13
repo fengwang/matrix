3c4044021b66135b5d764d63de1ed5821682c6ec
Zhang Chao <zhang_chao@live.com>
2012-04-13 16:34:55 +0200

    with flip-flap matrix-vector multiplication

--- include/matrix/operators/matrix_array_operators.hpp
+++ include/matrix/operators/matrix_array_operators.hpp
@@ -1,4 +1,5 @@
 #ifndef _MATRIX_ARRAY_OPERATORS_HPP_INCLUDED_DSFOIU4989RW8OISFDOIFSLKSFDJLKJVCXLKSLKJFSDLFSKOIEWEURSIDFUSLKJFEOIUFSDLIJDD
+
 #define _MATRIX_ARRAY_OPERATORS_HPP_INCLUDED_DSFOIU4989RW8OISFDOIFSLKSFDJLKJVCXLKSLKJFSDLFSKOIEWEURSIDFUSLKJFEOIUFSDLIJDD
 
 #include <matrix/matrix.hpp>
@@ -12,25 +13,27 @@
 namespace feng
 {
 
-    //matrix -- vector
     //matrix -- valarray
     //matrix -- ptr
+    //matrix -- vector
 
-    // matrix<double>   m;
-    // double*          p;
-    // valarray<double> v;
-    // vector<double>   a;
-    // auto b = m * p;
-    // auto b = m * v;
-    // auto b = m * a;
-    
     template<typename T, std::size_t D, typename A, typename T_>
     const matrix<T,D,A>
     operator * ( const matrix<T,D,A>& lhs,  const T_* const rhs )
-    {   //TODO: in case of optimization, parallel here
+    {   
         matrix<T,D,A> ans(lhs.row(), 1);
         for ( std::size_t i = 0; i < lhs.row(); ++i )
             ans[i][0] = std::inner_product( lhs.row_begin(i), lhs.row_end(i), rhs, T() );
+        return ans;
+    }
+
+    template<typename T, std::size_t D, typename A, typename T_>
+    const matrix<T,D,A>
+    operator * ( const T_* lhs, const matrix<T,D,A>& rhs )
+    {   
+        matrix<T,D,A> ans(1, rhs.col());
+        for ( std::size_t i = 0; i < rhs.col(); ++i )
+            ans[0][i] = std::inner_product( lhs, lhs+rhs.row(), rhs.col_begin(i), T() );
         return ans;
     }
     
@@ -47,12 +50,34 @@ namespace feng
     
     template<typename T, std::size_t D, typename A, typename T_>
     const matrix<T,D,A>
+    operator * ( const std::valarray<T_>& lhs, const matrix<T,D,A>& rhs )
+    {
+        assert( rhs.row() == lhs.size() );
+        matrix<T,D,A> ans(1, lhs.row());
+        for ( std::size_t i = 0; i < rhs.col(); ++i )
+            ans[0][i] = std::inner_product( std::begin(lhs), std::begin(lhs)+rhs.row(), rhs.col_begin(i), T() );
+        return ans;
+    }
+    
+    template<typename T, std::size_t D, typename A, typename T_>
+    const matrix<T,D,A>
     operator * ( const matrix<T,D,A>& lhs,  const std::vector<T_>& rhs )
     {
         assert( lhs.col() == rhs.size() );
         matrix<T,D,A> ans(lhs.row(), 1);
         for ( std::size_t i = 0; i < lhs.row(); ++i )
             ans[i][0] = std::inner_product( lhs.row_begin(i), lhs.row_end(i), rhs.begin(), T() );
+        return ans;
+    }
+    
+    template<typename T, std::size_t D, typename A, typename T_>
+    const matrix<T,D,A>
+    operator * ( const std::vector<T_>& lhs, const matrix<T,D,A>& rhs, )
+    {
+        assert( rhs.col() == lhs.size() );
+        matrix<T,D,A> ans(1, rhs.col());
+        for ( std::size_t i = 0; i < rhs.col(); ++i )
+            ans[0][i] = std::inner_product( lhs.begin(), lhs.end(), rhs.col_begin(i), T() );
         return ans;
     }
 
