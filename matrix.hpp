b639c88ca4c2cfad82dc0ed3f922194fb560f82c
Feng Wang <feng.wang@empa.ch>
2018-05-21 18:54:06 +0200

    remove old gauss jordan elimination code

--- matrix.hpp
+++ matrix.hpp
@@ -5608,81 +5608,6 @@ namespace feng
 
         return 0;
     }
-    template < typename T1, typename A1, typename T2, typename A2, typename T3, typename A3 >
-    int gauss_jordan_elimination( const matrix< T1, A1 >& A, matrix< T2, A2 >& x, const matrix< T3, A3 >& b )
-    {
-        typedef matrix< T1, A1 > matrix_type;
-        typedef typename matrix_type::size_type size_type;
-        typedef typename matrix_type::value_type value_type;
-        typedef typename matrix_type::range_type range_type;
-        assert( A.row() == A.col() );
-        assert( A.row() == b.row() );
-        matrix_type a( A || b );
-        const size_type n = b.row();
-        const size_type m = b.col();
-        struct abs_compare
-        {
-            bool operator()( value_type x, value_type y ) const
-            {
-                return std::abs( x ) < std::abs( y );
-            }
-        };
-        struct scale_by
-        {
-            value_type factor;
-            scale_by( value_type f )
-                : factor( f )
-            {
-            }
-            void operator()( value_type& x ) const
-            {
-                x /= factor;
-            }
-        };
-        struct ratio_by
-        {
-            value_type ratio;
-            ratio_by( value_type r )
-                : ratio( r )
-            {
-            }
-            value_type operator()( value_type x, value_type y ) const
-            {
-                return x - y * ratio;
-            }
-        };
-
-        for ( size_type i = 0; i < n; ++i )
-        {
-            const size_type p = std::distance( a.col_begin( i ), std::max_element( a.col_begin( i ) + i, a.col_end( i ), abs_compare() ) );
-
-            if ( p != i )
-                std::swap_ranges( a.row_begin( i ) + i, a.row_end( i ), a.row_begin( p ) + i );
-
-            const value_type factor = a[i][i];
-
-            if ( factor == value_type() ) return 1;
-
-            std::for_each( a.row_rbegin( i ), a.row_rend( i ) - i, scale_by( factor ) );
-
-            for ( size_type j = 0; j < n; ++j )
-            {
-                if ( i == j ) continue;
-
-                const value_type ratio = a[j][i];
-                std::transform( a.row_rbegin( j ), a.row_rend( j ) - i, a.row_rbegin( i ), a.row_rbegin( j ), ratio_by( ratio ) );
-            }
-        }
-
-        x = matrix_type( a, range_type( 0, n ), range_type( n, m + n ) );
-        return 0;
-    }
-
-    template < typename T1, typename A1, typename T2, typename A2, typename T3, typename A3 >
-    int gje( const matrix< T1, A1 >& A, matrix< T2, A2 >& x, const matrix< T3, A3 >& b )
-    {
-        return gauss_jordan_elimination( A, x, b );
-    }
 
     template < typename T, typename A >
     std::optional<matrix<T,A>> gauss_jordan_elimination( matrix< T, A > const& m ) noexcept
@@ -5701,19 +5626,15 @@ namespace feng
 
             auto const factor = a[i][i];
 
-            //if ( factor == value_type() ) return {};
             if ( std::abs(factor) < 1.0e-10) return {};
 
-            ///std::for_each( a.row_rbegin( i ), a.row_rend( i ) - i, scale_by( factor ) );
             std::for_each( a.row_rbegin( i ), a.row_rend( i ) - i, [factor](auto& v){ v /= factor; } );
 
-            //for ( size_type j = 0; j < row; ++j )
             for ( auto j : misc::range(row) )
             {
                 if ( i == j ) continue;
 
                 auto const ratio = a[j][i];
-                //std::transform( a.row_rbegin( j ), a.row_rend( j ) - i, a.row_rbegin( i ), a.row_rbegin( j ), ratio_by( ratio ) );
                 std::transform( a.row_rbegin( j ), a.row_rend( j ) - i, a.row_rbegin( i ), a.row_rbegin( j ), [ratio](auto x, auto y){ return x - y*ratio; } );
             }
         }
